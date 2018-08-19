3685606689f30c33e6996aa4742e8a4398543b38
seyfer <seyferseed@mail.ru>
2018-08-19 20:32:03 +0200

    refactoring

--- src/app/index.js
+++ src/app/index.js
@@ -7,75 +7,70 @@ function redraw() {
     document.querySelector('title').innerHTML = APP_NAME;
     document.querySelector('.title').innerHTML = APP_NAME;
 
-    const life_block = document.querySelector('#life');
-    life_block.innerHTML = '';
+    const lifeBlock = document.querySelector('#life');
+    lifeBlock.innerHTML = '';
     const now = new Date();
     const matches = window.location.hash.match(/#(\d{4})\.(\d{1,2})\.(\d{1,2})/);
 
-    let birth_year, birth_month, birth_day;
+    let birthYear, birthMonth, birthDay;
     if (matches && matches.length > 0) {
-        console.log(matches);
-        birth_year = parseInt(matches[1], 10);
-        birth_month = parseInt(matches[2], 10);
-        birth_day = parseInt(matches[3], 10);
+        birthYear = parseInt(matches[1], 10);
+        birthMonth = parseInt(matches[2], 10);
+        birthDay = parseInt(matches[3], 10);
     } else {
-        birth_year = prompt('What year were you born in?', 1980);
-        birth_month = prompt('What month(number) were you born in?', 1);
-        birth_day = prompt('What day were you born in?', 1);
+        birthYear = prompt('What year were you born in?', 1980);
+        birthMonth = prompt('What month(number) were you born in?', 1);
+        birthDay = prompt('What day were you born in?', 1);
     }
 
-    if (birth_year > 1900 && birth_year < now.getFullYear()) {
-    } else {
+    if (birthYear < 1900 || birthYear > now.getFullYear()) {
         alert('Wrong year!');
         return;
     }
 
-
-    if (birth_month > 0 && birth_month < 13) {
-    } else {
+    if (birthMonth < 1 || birthMonth > 12) {
         alert('Wrong month!');
         return;
     }
 
-
-    if (birth_day > 0 && birth_day < 32) {
-    } else {
+    if (birthDay < 1 || birthDay > 31) {
         alert('Wrong day!');
         return;
     }
 
-    const birthdate = Date.UTC(birth_year, birth_month, birth_day);
+    const birthDate = Date.UTC(birthYear, birthMonth, birthDay);
     const today = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
-    const weeks = (today - birthdate) / (1000 * 60 * 60 * 24 * 7);
+    const weeks = (today - birthDate) / (1000 * 60 * 60 * 24 * 7);
 
-    let week_of_life = 0;
-    let div, week, year_number, week_class, title;
+    let weekOfLife = 0;
+    let div, week, yearNumber, weekClass, title;
     for (let i = 0; i <= 80; i++) {
         div = document.createElement('div');
         div.setAttribute('id', 'year_' + i);
         div.setAttribute('class', 'year');
-        year_number = document.createElement('div');
-        year_number.setAttribute('class', 'year_number');
-        year_number.innerHTML = i;
-        div.appendChild(year_number);
+        yearNumber = document.createElement('div');
+        yearNumber.setAttribute('class', 'year_number');
+        yearNumber.innerHTML = i;
+        div.appendChild(yearNumber);
         for (let j = 1; j <= 52; j++) {
-            week_of_life++;
+            weekOfLife++;
             title = '';
             week = document.createElement('div');
-            week.setAttribute('id', 'week_' + week_of_life);
+            week.setAttribute('id', 'week_' + weekOfLife);
 
-            week_class = 'week week_of_year_' + j;
-            if (week_of_life <= weeks) {
-                week_class += ' ended';
+            weekClass = 'week week_of_year_' + j;
+            if (weekOfLife <= weeks) {
+                weekClass += ' ended';
             }
-            if (week_of_life === Math.ceil(weeks)) {
-                week_class += ' current';
+            if (weekOfLife === Math.ceil(weeks)) {
+                weekClass += ' current';
             }
-            week.setAttribute('title', title + week_of_life + ' week of life and ' + j + ' week of ' + i + ' year.');
-            week.setAttribute('class', week_class);
+            week.setAttribute('title', title + weekOfLife + ' week of life and ' + j + ' week of ' + i + ' year.');
+            week.setAttribute('class', weekClass);
             div.appendChild(week);
         }
-        life_block.appendChild(div);
+
+        lifeBlock.appendChild(div);
     }
 }
 
