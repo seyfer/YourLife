1f7795db63dff929cf1440dab643172e903843d4
seyfer <seyferseed@mail.ru>
2018-08-27 14:56:05 +0200

    refactoring

--- src/app/index.js
+++ src/app/index.js
@@ -2,81 +2,93 @@
 
 import '../style/app.scss';
 
-function redraw() {
-
-    if (typeof APP_NAME !== 'undefined') {
-        document.querySelector('title').innerHTML = APP_NAME;
-        document.querySelector('.title').innerHTML = APP_NAME;
+class YourLifeManager {
+    constructor() {
+        this.events();
     }
 
-    const lifeBlock = document.querySelector('#life');
-    lifeBlock.innerHTML = '';
-    const now = new Date();
-    const matches = window.location.hash.match(/#(\d{4})\.(\d{1,2})\.(\d{1,2})/);
+    events() {
+        const self = this;
 
-    let birthYear, birthMonth, birthDay;
-    if (matches && matches.length > 0) {
-        birthYear = parseInt(matches[1], 10);
-        birthMonth = parseInt(matches[2], 10);
-        birthDay = parseInt(matches[3], 10);
-    } else {
-        birthYear = prompt('What year were you born in?', 1980);
-        birthMonth = prompt('What month(number) were you born in?', 1);
-        birthDay = prompt('What day were you born in?', 1);
+        document.addEventListener('DOMContentLoaded', function () {
+            self.redraw();
+            window.onhashchange = self.redraw;
+        });
     }
 
-    if (birthYear < 1900 || birthYear > now.getFullYear()) {
-        alert('Wrong year!');
-        return;
-    }
+    redraw() {
 
-    if (birthMonth < 1 || birthMonth > 12) {
-        alert('Wrong month!');
-        return;
-    }
-
-    if (birthDay < 1 || birthDay > 31) {
-        alert('Wrong day!');
-        return;
-    }
-
-    const birthDate = Date.UTC(birthYear, birthMonth, birthDay);
-    const today = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
-    const weeks = (today - birthDate) / (1000 * 60 * 60 * 24 * 7);
-
-    let weekOfLife = 0;
-    let div, week, yearNumber, weekClass, title;
-    for (let i = 0; i <= 80; i++) {
-        div = document.createElement('div');
-        div.setAttribute('id', 'year_' + i);
-        div.setAttribute('class', 'year');
-        yearNumber = document.createElement('div');
-        yearNumber.setAttribute('class', 'year_number');
-        yearNumber.innerHTML = i;
-        div.appendChild(yearNumber);
-        for (let j = 1; j <= 52; j++) {
-            weekOfLife++;
-            title = '';
-            week = document.createElement('div');
-            week.setAttribute('id', 'week_' + weekOfLife);
-
-            weekClass = 'week week_of_year_' + j;
-            if (weekOfLife <= weeks) {
-                weekClass += ' ended';
-            }
-            if (weekOfLife === Math.ceil(weeks)) {
-                weekClass += ' current';
-            }
-            week.setAttribute('title', title + weekOfLife + ' week of life and ' + j + ' week of ' + i + ' year.');
-            week.setAttribute('class', weekClass);
-            div.appendChild(week);
+        if (typeof APP_NAME !== 'undefined') {
+            document.querySelector('title').innerHTML = APP_NAME;
+            document.querySelector('.title').innerHTML = APP_NAME;
         }
 
-        lifeBlock.appendChild(div);
+        const lifeBlock = document.querySelector('#life');
+        lifeBlock.innerHTML = '';
+        const now = new Date();
+        const matches = window.location.hash.match(/#(\d{4})\.(\d{1,2})\.(\d{1,2})/);
+
+        let birthYear, birthMonth, birthDay;
+        if (matches && matches.length > 0) {
+            birthYear = parseInt(matches[1], 10);
+            birthMonth = parseInt(matches[2], 10);
+            birthDay = parseInt(matches[3], 10);
+        } else {
+            birthYear = prompt('What year were you born in?', 1980);
+            birthMonth = prompt('What month(number) were you born in?', 1);
+            birthDay = prompt('What day were you born in?', 1);
+        }
+
+        if (birthYear < 1900 || birthYear > now.getFullYear()) {
+            alert('Wrong year!');
+            return;
+        }
+
+        if (birthMonth < 1 || birthMonth > 12) {
+            alert('Wrong month!');
+            return;
+        }
+
+        if (birthDay < 1 || birthDay > 31) {
+            alert('Wrong day!');
+            return;
+        }
+
+        const birthDate = Date.UTC(birthYear, birthMonth, birthDay);
+        const today = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
+        const weeks = (today - birthDate) / (1000 * 60 * 60 * 24 * 7);
+
+        let weekOfLife = 0;
+        let div, week, yearNumber, weekClass, title;
+        for (let i = 0; i <= 80; i++) {
+            div = document.createElement('div');
+            div.setAttribute('id', 'year_' + i);
+            div.setAttribute('class', 'year');
+            yearNumber = document.createElement('div');
+            yearNumber.setAttribute('class', 'yearNumber');
+            yearNumber.innerHTML = i;
+            div.appendChild(yearNumber);
+            for (let j = 1; j <= 52; j++) {
+                weekOfLife++;
+                title = '';
+                week = document.createElement('div');
+                week.setAttribute('id', 'week_' + weekOfLife);
+
+                weekClass = '';
+                if (weekOfLife <= weeks) {
+                    weekClass += ' ended';
+                }
+                if (weekOfLife === Math.ceil(weeks)) {
+                    weekClass += ' current';
+                }
+                week.setAttribute('title', title + weekOfLife + ' week of life and ' + j + ' week of ' + i + ' year.');
+                if (weekClass.length > 0) week.setAttribute('class', weekClass);
+                div.appendChild(week);
+            }
+
+            lifeBlock.appendChild(div);
+        }
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    redraw();
-    window.onhashchange = redraw;
-});
+new YourLifeManager();
